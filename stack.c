bc40aeaa0d45b91808bc648d6302e829d1329cd4
MohamedTwahir <mohamedtwahir977@gmail.com>
2023-11-19 06:27:39 -0800

    stack data structure

---
+++ stack.c
@@ -0,0 +1,13 @@
+#include "monty.h"
+/**
+ * f_stack - prints stack top
+ * @head: head of stack
+ * @counter: number of lines counted
+ * Return: nothing to return
+ */
+void f_stack(stack_t *head, unsigned int counter)
+{
+	(void)head;
+	(void)counter;
+	bus.lifi = 0;
+}
