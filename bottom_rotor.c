7e0d79d357556df3b1bd5874a7d372069fb14932
MohamedTwahir <mohamedtwahir977@gmail.com>
2023-11-19 06:24:53 -0800

    function that rotates stack bottom

---
+++ bottom_rotor.c
@@ -0,0 +1,26 @@
+#include "monty.h"
+/**
+ * f_rotr - rotates stack bottom
+ * @head: stack head
+ * @counter: number of lines counted
+ * Return: nothing to return
+ */
+void f_rotr(stack_t **head, __attribute__((unused)) unsigned int counter)
+{
+	stack_t *copy;
+
+	copy = *head;
+	if (*head == NULL || (*head)->next == NULL)
+	{
+		return;
+	}
+	while (copy->next)
+	{
+		copy = copy->next;
+	}
+	copy->next = *head;
+	copy->prev->next = NULL;
+	copy->prev = NULL;
+	(*head)->prev = copy;
+	(*head) = copy;
+}
